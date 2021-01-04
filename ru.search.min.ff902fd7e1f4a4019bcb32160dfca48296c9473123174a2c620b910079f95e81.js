f64f6af33b4e2b3fbf617cbe1c978b8cde056e63
infoanarchy <76272657+infoanarchy@users.noreply.github.com>
2021-01-04 14:39:43 +0300

    Удаление устаревшей статьи, починка меню и картинок в статье про nas, добавление способа скачивания потокового видео.

--- ru.search.min.ff902fd7e1f4a4019bcb32160dfca48296c9473123174a2c620b910079f95e81.js
+++ ru.search.min.ff902fd7e1f4a4019bcb32160dfca48296c9473123174a2c620b910079f95e81.js
@@ -3,7 +3,7 @@ input.addEventListener('focus',init);input.addEventListener('keyup',search);docu
 const characterPressed=String.fromCharCode(event.charCode);if(!isHotkey(characterPressed)){return;}
 input.focus();event.preventDefault();}
 function isHotkey(character){const dataHotkeys=input.getAttribute('data-hotkeys')||'';return dataHotkeys.indexOf(character)>=0;}
-function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/en.search-data.min.88ae6d431b509caf83a64b2452a0df22301105ccd13bfff3d901d18ab092410b.js',function(){input.required=false;search();});}
+function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/ru.search-data.min.31af51625e10749ae57e83d8d04c78f36a760d7c0ccc6ab9ae19a30a45a31578.js',function(){input.required=false;search();});}
 function search(){while(results.firstChild){results.removeChild(results.firstChild);}
 if(!input.value){return;}
 const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=element('<li><a href></a><small></small></li>');const a=li.querySelector('a'),small=li.querySelector('small');a.href=page.href;a.textContent=page.title;small.textContent=page.section;results.appendChild(li);});}
